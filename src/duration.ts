f1bf367b77f665f3e73f35a46880c1fa71f14868
Erion Jasiqi <erion_jasiqi@sluz.ch>
2025-04-14 22:24:16 +0200

    Task 1: finished adding the formatDuration function

--- src/duration.ts
+++ src/duration.ts
@@ -1,4 +1,20 @@
 export function formatDuration(seconds: number): string {
-  console.log(seconds);
-  throw new Error("not implemented yet");
+  const s = Math.round(seconds);
+  if (s < 0) throw new Error("Seconds cant be negative");
+
+  const h = Math.floor(s / 3600);
+  const m = Math.floor((s % 3600) / 60);
+  const sec = s % 60;
+  let result = "";
+
+  if (h > 0) {
+    result += `${h}h`;
+  }
+  if (m > 0) {
+    result += `${m}m`;
+  }
+  if (sec > 0 || result === "") {
+    result += `${sec}s`;
+  }
+  return result;
 }
